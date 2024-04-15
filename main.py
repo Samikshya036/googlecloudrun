6e9d1f74a00ddab49e7d5691887573b81e7800de
Samikshya036 <113791186+Samikshya036@users.noreply.github.com>
2024-04-14 21:27:56 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,20 @@
-import os
+from flask import Flask, jsonify
 import psycopg2
-from flask import Flask
+from shapely.wkb import loads
+
+# PostGIS database connection details
+dbname = 'gis5572'
+user = 'postgres'
+password = 'sami@2010'
+host = '35.202.65.52'  # Cloud DB Public IP address
+port = '5432'
 
 app = Flask(__name__)
 
-
-@app.route("/")
-def hello_world():
-    return "Yepuuuuu!"
-
-@app.route("/hello")
-def hello():
-    return "hello"
-
-if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
+def connect_to_postgres():
+    try:
+        connection = psycopg2.connect(dbname=dbname, user=user, password=password, host=host, port=port)
+        return connection
+    except psycopg2.Error as e:
+        print(f"Error connecting to PostgreSQL: {e}")
+        return None
